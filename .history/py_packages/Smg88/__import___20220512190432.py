f0270258d693b220745686f348c1ae2363523cfb
Smartguy88 - Smg88 <96052597+Smartestguy88@users.noreply.github.com>
2022-05-14 17:28:29 +1000

    setting up IOT

--- .history/py_packages/Smg88/__import___20220512190432.py
+++ .history/py_packages/Smg88/__import___20220512190432.py
@@ -5,18 +5,4 @@ If their is any static reference to any part of the Smg88 package (standalone fi
 All files in Smg88 module is designed to be runnable in a standalone fashion, this file manages which files could be imported and which not to determine dynamic dependencies
 """
 
-from types import ModuleType
-
-
 print("Somebody ran __import__.py! Yay!")
-
-__expand = True
-
-if __name__ == "__main__":
-    print("__import__.py is being run directly! Oh ooooh!")
-    __expand = False
-
-if __expand:
-    __location__ = __file__
-
-    ModuleType
